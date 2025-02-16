82ba8242b5f16d122dc9a852fa4decafe72a54f7
eldadstar <eldadfikre456@gmail.com>
2025-02-16 17:47:19 +0300

    Joi Validator Decorator

---
+++ backend/src/shared/globals/decorators/joi-validation.decorators.ts
@@ -0,0 +1,26 @@
+import { JoiRequestValidationError } from "../helpers/error.handler";
+import { Request } from "express";
+import { ObjectSchema } from "joi";
+
+type IJoiDecotator = (target: any, key: string, descriptor: PropertyDescriptor) => void;
+
+export function joiValidation(schema: ObjectSchema): IJoiDecotator {
+  return (_target: any, _key: string, descriptor: PropertyDescriptor) => {
+    const originalMethod = descriptor.value;
+
+    descriptor.value = async function (...args: any[]){
+      const req: Request = args[0];
+      const {error} = await Promise.resolve(schema.validate(req.body));
+      if(error?.details){
+        throw new JoiRequestValidationError(error.details[0].message)
+      }
+      return originalMethod.apply(this, args);
+    }
+    return descriptor;
+  }
+}
+
+function signup(req,res,next) {
+
+}
+
