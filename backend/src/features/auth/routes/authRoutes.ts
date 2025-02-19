594405ab4663590806006d22c21cc0aeecb04ff9
eldadstar <eldadfikre456@gmail.com>
2025-02-20 02:32:04 +0300

    Forgot password

--- backend/src/features/auth/routes/authRoutes.ts
+++ backend/src/features/auth/routes/authRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { SignUp } from '../controllers/signup';
 import { SignIn } from '../controllers/signIn';
 import { SignOut } from '../controllers/signout';
+import { Password } from '../controllers/password';
 
 class AuthRoutes {
   private router: Router;
@@ -13,6 +14,7 @@ class AuthRoutes {
   public routes(): Router {
     this.router.post('/signup', SignUp.prototype.create);
     this.router.post('/signin', SignIn.prototype.read);
+    this.router.post('/forgot-password', Password.prototype.create);
 
     return this.router;
   }
