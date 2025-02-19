866fc27ca2b6f5dbf159671e6647c40455cb158f
eldadstar <eldadfikre456@gmail.com>
2025-02-20 02:09:15 +0300

    Test Email

--- backend/src/shared/services/emails/templates/forgot-password/forgot-password-template.ts
+++ backend/src/shared/services/emails/templates/forgot-password/forgot-password-template.ts
@@ -1,15 +1,15 @@
-import fs from 'fs';
-import ejs from 'ejs';
+import fs from 'fs'
+import ejs from 'ejs'
 
-
-class ForgetPasswordTemplate {
-public passwordResetTemplate(username: string, resetLink: string){
-  return ejs.render(fs.readFileSync(__dirname + '/forgot-password-template.ejs', 'utf8'), {
-    username,
-    resetLink,
-    image_url: 'https://www.clipartmax.com/png/small/421-4210885_lock-svg-png-icon-free-download-free-lock-icon-png.png'
-  });
+class ForgotPasswordTemplate {
+  public passwordResetTemplate(username: string, resetLink: string): string {
+    return ejs.render(fs.readFileSync(__dirname + '/forgot-password-template.ejs', 'utf8'), {
+      username,
+      resetLink,
+      image_url:
+        'https://w7.pngwing.com/pngs/120/102/png-transparent-padlock-logo-computer-icons-padlock-technic-logo-password-lock.png'
+    })
   }
 }
 
-export const forgetPasswordTemplate: ForgetPasswordTemplate = new ForgetPasswordTemplate();
+export const forgotPasswordTemplate: ForgotPasswordTemplate = new ForgotPasswordTemplate()
