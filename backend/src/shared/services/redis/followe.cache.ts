8a7ff4cab492de8a6026fea668b1822aef85d198
eldadstar <eldadfikre456@gmail.com>
2025-02-23 17:09:10 +0300

    Follower Redis Cache

---
+++ backend/src/shared/services/redis/followe.cache.ts
@@ -0,0 +1,52 @@
+import { IUserDocument } from '@root/features/user/interfaces/user.interface';
+import { BaseCache } from './base.cache';
+import Logger from 'bunyan';
+import { find } from 'lodash';
+import { config } from '@root/config';
+import { ServerError } from '@root/shared/globals/helpers/error.handler';
+
+const log: Logger = config.createLogger('followersCache');
+
+export class FollowerCache extends BaseCache {
+  constructor() {
+    super('followersCache');
+  }
+
+  public async saveFollowerToCache(key: string, value: string): Promise<void> {
+   try{
+    if(!this.client.isOpen){
+          await this.client.connect();
+      }
+
+      await this.client.LPUSH(key, value);
+    } catch(error){
+      log.error(error);
+      throw new ServerError('Server Error. Try again Please')
+    }
+  }
+
+  public async removeFollowerFromCache(key: string, value: string): Promise<void> {
+    try{
+     if(!this.client.isOpen){
+           await this.client.connect();
+       }
+
+       await this.client.LREM(key, 1, value);
+     } catch(error){
+       log.error(error);
+       throw new ServerError('Server Error. Try again Please')
+     }
+   }
+
+   public async updateFollowerFromCache(userId: string, prop: string, value: number): Promise<void> {
+    try{
+     if(!this.client.isOpen){
+           await this.client.connect();
+       }
+       await this.client.HINCRBY(`users:${userId}`, prop, value);
+     } catch(error){
+       log.error(error);
+       throw new ServerError('Server Error. Try again Please')
+     }
+   }
+}
