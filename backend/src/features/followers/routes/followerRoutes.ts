df42c4aa3d8c151af8a23371a29cff111909b236
eldadstar <eldadfikre456@gmail.com>
2025-02-24 12:10:15 +0300

    Get Followers controllers

--- backend/src/features/followers/routes/followerRoutes.ts
+++ backend/src/features/followers/routes/followerRoutes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { authMiddleware } from '@root/shared/globals/helpers/auth-middleware';
 import { Add } from '../controller/follower-user';
 import { Remove } from '../controller/unfollow-user';
+import { GetFollow } from '../controller/get-followers';
 
 class FollowerRoutes {
   private router: Router;
@@ -13,6 +14,10 @@ class FollowerRoutes {
   public routes(): Router {
     this.router.put('/user/follow/:followerId', authMiddleware.checkAuthentication, Add.prototype.follower);
     this.router.put('/user/unfollow/:followeeId/:followerId', authMiddleware.checkAuthentication, Remove.prototype.follower);
+
+
+    this.router.get('/user/following', authMiddleware.checkAuthentication, GetFollow.prototype.userFollowing);
+    this.router.get('/user/followers/:userId', authMiddleware.checkAuthentication, GetFollow.prototype.userFollowers);
 
     return this.router;
   }
