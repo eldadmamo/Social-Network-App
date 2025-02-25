29ecec2f1f74c36b880271df3d39b0b9297b1ee4
eldadstar <eldadfikre456@gmail.com>
2025-02-25 19:05:44 +0300

    Chat message to Redis

--- backend/src/features/chat/controllers/add-chat-message.ts
+++ backend/src/features/chat/controllers/add-chat-message.ts
@@ -83,6 +83,7 @@ export class Add {
 
     await messageCache.addChatListToCache(`${req.currentUser!.userId}`, `${receiverId}`, `${conversationObjectId}`);
     await messageCache.addChatListToCache(`${receiverId}`, `${req.currentUser!.userId}`, `${conversationObjectId}`);
+    await messageCache.addChatMessageToCache(`${conversationObjectId}`, messageData);
 
     res.status(HTTP_STATUS.OK).json({message: 'Message added', conversationId:conversationObjectId})
   }
--- backend/src/shared/services/redis/message.cache.ts
+++ backend/src/shared/services/redis/message.cache.ts
@@ -3,6 +3,7 @@ import Logger from "bunyan";
 import { findIndex } from "lodash";
 import { config } from "@root/config";
 import { ServerError } from "@global/helpers/error.handler";
+import { IMessageData } from "@chat/interfaces/chat.interface";
 
 const log: Logger = config.createLogger('messageCache');
 
@@ -25,6 +26,19 @@ public async addChatListToCache(senderId: string, receiverId: string, conversati
           await this.client.RPUSH(`chatList:${senderId}`, JSON.stringify({receiverId, conversationId}));
         }
       }
+    }catch(error){
+      log.error(error);
+      throw new ServerError('Server error. Try again')
+    }
+}
+
+public async addChatMessageToCache(conversationId: string, value:IMessageData): Promise<void> {
+  try{
+      if(!this.client.isOpen){
+        await this.client.connect();
+      }
+
+      await this.client.RPUSH(`messages:${conversationId}`, JSON.stringify(value));
     }catch(error){
       log.error(error);
       throw new ServerError('Server error. Try again')
