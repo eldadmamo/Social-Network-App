0f59b30c0658763851936e7ddab159aa03ff7a17
eldadstar <eldadfikre456@gmail.com>
2025-02-27 14:11:39 +0300

    Get users redis cache

--- backend/src/shared/services/redis/user.cache.ts
+++ backend/src/shared/services/redis/user.cache.ts
@@ -4,9 +4,12 @@ import Logger from 'bunyan';
 import { config } from '@root/config';
 import { ServerError } from '@root/shared/globals/helpers/error.handler';
 import { Helpers } from '@root/shared/globals/helpers/helpers';
+import { RedisCommandRawReply } from '@redis/client/dist/lib/commands';
 
 const log: Logger = config.createLogger('userCache');
 type UserItem = string | ISocialLinks | INotificationSettings;
+type UserCacheMultiType = string | number | Buffer | RedisCommandRawReply[] | IUserDocument | IUserDocument[];
+
 
 export class UserCache extends BaseCache {
   constructor() {
@@ -126,6 +129,52 @@ export class UserCache extends BaseCache {
       throw new ServerError('Server Error. Try Again');
     }
   }
+
+  public async getUsersFromCache(start: number, end: number, excludedUserKey: string): Promise<IUserDocument[]> {
+    try {
+        if (!this.client.isOpen) {
+            await this.client.connect();
+        }
+        const response: string[] = await this.client.ZRANGE('user', start, end);
+        response.reverse();
+
+        const multi: ReturnType<typeof this.client.multi> = this.client.multi();
+
+        for (const key of response) {
+            if (key != excludedUserKey) {
+                multi.HGETALL(`users:${key}`);
+            }
+        }
+        const replies: UserCacheMultiType = await multi.exec() as UserCacheMultiType;
+        const userReplies: IUserDocument[] = [];
+
+        for (const reply of replies as IUserDocument[]) {
+            reply.createdAt = new Date(Helpers.parseJson(`${reply.createdAt}`));
+            reply.postsCount = Helpers.parseJson(`${reply.postsCount}`);
+            reply.blocked = Helpers.parseJson(`${reply.blocked}`);
+            reply.blockedBy = Helpers.parseJson(`${reply.blockedBy}`);
+            reply.notifications = Helpers.parseJson(`${reply.notifications}`);
+            reply.social = Helpers.parseJson(`${reply.social}`);
+            reply.followersCount = Helpers.parseJson(`${reply.followersCount}`);
+            reply.followingCount = Helpers.parseJson(`${reply.followingCount}`);
+            reply.bgImageId = Helpers.parseJson(`${reply.bgImageId}`);
+            reply.bgImageVersion = Helpers.parseJson(`${reply.bgImageVersion}`);
+            reply.profilePicture = Helpers.parseJson(`${reply.profilePicture}`);
+            reply.work = Helpers.parseJson(`${reply.work}`);
+            reply.school = Helpers.parseJson(`${reply.school}`);
+            reply.location = Helpers.parseJson(`${reply.location}`);
+            reply.quote = Helpers.parseJson(`${reply.quote}`);
+
+            userReplies.push(reply);
+        }
+
+        return userReplies;
+
+    } catch (error) {
+        log.error(error);
+        throw new ServerError('Server error. Try again.');
+    }
+}
 
 
   public async updateSingleUserItemInCache(userId: string, prop: string, value: UserItem): Promise<IUserDocument | null> {
