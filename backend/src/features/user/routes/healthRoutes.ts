04dbf3ec6163cf57a6e51784b247d670b931b736
eldadstar <eldadfikre456@gmail.com>
2025-02-28 17:23:11 +0300

    Health Routes

--- backend/src/features/user/routes/healthRoutes.ts
+++ backend/src/features/user/routes/healthRoutes.ts
@@ -39,6 +39,36 @@ class HealthRoutes {
 
     return this.router;
   }
+
+  public fiboRoutes(): Router {
+    this.router.get('/fibo/:num', async (req: Request, res: Response) => {
+      const {num} = req.params;
+      const start: number = performance.now();
+      const result: number = this.fibo(parseInt(req.params.num, 10));
+      const end: number = performance.now();
+      // const response = await axios({
+      //   method: 'get',
+      //   url: config.EC2_URL
+      // })
+      res.status(HTTP_STATUS.OK).send(
+        `fibonacci seried of ${num} is ${result} and it took ${end-start}ms with EC2 instance of
+        and process id ${process.pid} on ${moment().format('LL')}
+        `
+      );
+    });
+
+    return this.router;
+  }
+
+  private fibo(data: number): number {
+    if(data < 2){
+      return 1;
+    } else {
+      return this.fibo(data - 2) + this.fibo(data -1);
+    }
+  }
+
+
 }
 
 export const healthRoutes: HealthRoutes = new HealthRoutes();
