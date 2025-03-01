e1bf44513045f9d7d4bb4e8bb83ab525335e07ac
eldadstar <eldadfikre456@gmail.com>
2025-03-01 23:39:13 +0300

    VPC and Subnets

---
+++ backend/deployment/4-vpc.tf
@@ -0,0 +1,13 @@
+resource "aws-vpc" "main" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_hostnames = true
+
+  tags = merge(
+  local.common_tags,
+  tomap({"Name"= "${local.prefix}"})
+)
+}
+
+
+
+
