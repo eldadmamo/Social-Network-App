887a6e1a8370ce2a71774ad95cc9243b4bac4b22
eldadstar <eldadfikre456@gmail.com>
2025-03-03 00:02:48 +0300

    Button and input componenets

--- frontend/src/pages/auth/auth-tabs/AuthTabs.jsx
+++ frontend/src/pages/auth/auth-tabs/AuthTabs.jsx
@@ -1,28 +1,37 @@
 import './AuthTabs.scss'
 import React from 'react'
+import { useState } from 'react'
+import backgroundImage from '../../../assets/images/background.jpg'
 
 const AuthTabs = () => {
+  const [type, setType] = useState('Sign In');
+
+
   return (
     <>
-      <div className='container-wrapper'>
-        <div className='enviroment'>Dev</div>
+      <div className='container-wrapper' style={{backgroundImage: `url(${backgroundImage})` }}>
+        <div className='environment'>Dev</div>
         <div className='container-wrapper-auth'>
           <div className='tabs'>
             <div className='tabs-auth'>
               <ul className='tab-group'>
-                <li className='tab active'>
+                <li className='tab active' onClick={()=> setType('Sign In')}>
                   <button className='login'>Sign In</button>
                 </li>
                 <li className='tab'>
-                  <button className='signup'>Sign Up</button>
+                  <button className='signup' onClick={()=> setType('Sign Up')}>Sign Up</button>
                 </li>
               </ul>
-              <div className='tab-item'>
-                login components
-              </div>
-              <div className='tab-item'>
-                register components
-              </div>
+              {
+                type === 'Sign In' && <div className='tab-item'>
+                  login components
+                </div>
+              }
+              {
+                type === 'Sign Up' && <div className='tab-item'>
+                  signup components
+                </div>
+              }
             </div>
           </div>
         </div>
