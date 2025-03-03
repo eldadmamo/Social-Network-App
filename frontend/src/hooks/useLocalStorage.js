0e073b06fb072be3153d29a0802594c2ffacfa7c
eldadstar <eldadfikre456@gmail.com>
2025-03-04 01:18:06 +0300

    localstorage and sessionstorage

---
+++ frontend/src/hooks/useLocalStorage.js
@@ -0,0 +1,23 @@
+const useLocationStorage = (key,type) => {
+    try{
+        if(type === 'get'){
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : '';
+        } else if (type === 'set'){
+            const setValue = (newValue) => {
+                window.localStorage.setItem(key,JSON.stringify(newValue))
+            }
+            return [setValue];
+        } else {
+            const deleteValue = () => {
+                window.localStorage.removeItem(key);
+            }
+            return [deleteValue];
+        }
+    }catch(error){
+        console.log(error);
+    }
+};
+
+export default useLocationStorage;
+
