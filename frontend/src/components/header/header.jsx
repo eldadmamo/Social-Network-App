38339c380c61ee23fffcf6ca2ea57534bd081c7d
eldadstar <eldadfikre456@gmail.com>
2025-03-04 23:40:15 +0300

    Message Sidebar

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,14 +1,22 @@
 import logo from '../../assets/images/logo.svg';
 import { FaCaretDown, FaRegBell, FaRegEnvelope } from 'react-icons/fa';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './Header.scss';
 import Avatar from '../avatar/Avatar';
 import { Utils } from '../../services/utils/utils.service';
+import useDetectOutsideClick from '../../hooks/useDetectOutsideClick';
+import MessageSidebar from '../message-sidebar/MessageSidebar';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const {profile} = useSelector(state => state.user);
     const [enviroment, setEnviroment] = useState('');
+    const messageRef = useRef(null);
+    const [isMessageActive, setIsMessageActive] = useDetectOutsideClick(messageRef, false);
 
     const backgroundColor = `${enviroment === 'DEV' ? '#50b5ff': enviroment === 'STG' ? '#e9710f': ''}`
+
+    const openChatPage = () => {}
 
     useEffect(()=> {
         const env = Utils.appEnviroment();
@@ -17,6 +25,16 @@ const Header = () => {
   return (
     <>
       <div className="header-nav-wrapper" data-testid="header-wrapper">
+        {isMessageActive && (
+            <div ref={messageRef}>
+                <MessageSidebar
+                profile={profile}
+                messageCount={0}
+                messageNotifications={[]}
+                openChatPage={openChatPage}
+                />
+            </div>
+        )}
           <div className="header-navbar">
             <div className="header-image" data-testid="header-image">
               <img src={logo} className="img-fluid" alt="" />
@@ -46,7 +64,9 @@ const Header = () => {
                 </ul>
                 &nbsp;
               </li>
-              <li className="header-nav-item active-item">
+              <li className="header-nav-item active-item" onClick={()=> {
+                setIsMessageActive(true)
+              }}>
                 <span className="header-list-name">
                   <FaRegEnvelope className="header-list-icon" />
                   <span className="bg-danger-dots dots" data-testid="messages-dots"></span>
