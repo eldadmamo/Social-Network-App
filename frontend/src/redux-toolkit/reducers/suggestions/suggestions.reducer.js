87b3bb6f4b2d54edfbad3829816c2310ad17ceb1
eldadstar <eldadfikre456@gmail.com>
2025-03-04 20:00:24 +0300

    Add users Suggestions

--- frontend/src/redux-toolkit/reducers/suggestions/suggestions.reducer.js
+++ frontend/src/redux-toolkit/reducers/suggestions/suggestions.reducer.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { getUserSuggestions } from "../../api/suggestion";
+import { TRUE } from "sass";
 
 const initialState = {
     users: [],
@@ -14,6 +16,19 @@ const suggestionsSlice = createSlice({
             state.users = [...users];
             state.isLoading = isLoading;
         }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(getUserSuggestions.pending, (state) => {
+            state.isLoading = true;
+        }),
+        builder.addCase(getUserSuggestions.fulfilled, (state, action) => {
+            state.isLoading = false;
+            const {users} = action.payload;
+            state.users = [...users];
+        })
+        builder.addCase(getUserSuggestions.rejected, (state) => {
+            state.isLoading = false;
+        })
     }
 })
 
