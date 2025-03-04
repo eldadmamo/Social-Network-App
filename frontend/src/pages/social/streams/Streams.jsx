3334b445f3690a4bdf000feb9b559c23118a5c2f
eldadstar <eldadfikre456@gmail.com>
2025-03-04 19:14:32 +0300

    Streams Page

--- frontend/src/pages/social/streams/Streams.jsx
+++ frontend/src/pages/social/streams/Streams.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useRef } from 'react'
+import './Streams.scss'
 
 const Streams = () => {
+  const bodyRef = useRef(null);
+  const bottomLineRef = useRef();
+
   return (
-    <div>
-      Streams Components
+    <div className="streams" data-testid="streams">
+      <div className="streams-content">
+        <div className="streams-post" ref={bodyRef} style={{backgroundColor: 'white'}}>
+          <div>User Form</div>
+          <div ref={bottomLineRef} style={{ marginBottom: '50px', height: '50px' }}></div>
+        </div>
+        <div className="streams-suggestions">
+          <div>Suggession</div>
+        </div>
+      </div>
     </div>
   )
 }
