39aee11ac8a5c460b48659445235bc51dc0cc31f
eldadstar <eldadfikre456@gmail.com>
2025-03-05 13:00:36 +0300

    logout user

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -6,11 +6,14 @@ import Avatar from '../avatar/Avatar';
 import { Utils } from '../../services/utils/utils.service';
 import useDetectOutsideClick from '../../hooks/useDetectOutsideClick';
 import MessageSidebar from '../message-sidebar/MessageSidebar';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Dropdown from '../dropdown/Dropdown';
 import useEffectOnce from './../../hooks/useEffectOnce';
 import { ProfileUtils } from '../../services/utils/profile-utils.service';
 import { useNavigate } from 'react-router-dom';
+import useLocalStorage from './../../hooks/useLocalStorage';
+import useSessionStorage from './../../hooks/useSessionStorage';
+import { userService } from '../../services/api/user/user.service';
 
 const Header = () => {
     const {profile} = useSelector(state => state.user);
@@ -20,17 +23,29 @@ const Header = () => {
     const notificationRef = useRef(null);
     const settingsRef = useRef(null);
     const navigate = useNavigate();
+    const dispatch = useDispatch();
     const [isMessageActive, setIsMessageActive] = useDetectOutsideClick(messageRef, false);
     const [isNotificationActive, setNotificationActive] = useDetectOutsideClick(notificationRef, false);
     const [isSettingsActive, setIsSettingsActive] = useDetectOutsideClick(settingsRef, false);
-    
+    const [deleteStorageUsername] = useLocalStorage('username', 'delete')
+    const [setLoggedIn] = useLocalStorage('keepLoggedIn', 'set')
+    const [deleteSessionPagePayload] = useSessionStorage('pageReload', 'delete')
 
     const backgroundColor = `${enviroment === 'DEV' ? '#50b5ff': enviroment === 'STG' ? '#e9710f': ''}`
 
     const openChatPage = () => {}
     const onMarkAsRead = () => {}
     const onDeleteNotification = () => {}
-    const onLogout = () => {} 
+    const onLogout = async () => {
+      try{
+        setLoggedIn(false);
+        Utils.clearStore({dispatch, deleteStorageUsername, deleteSessionPagePayload, setLoggedIn});
+        await userService.logoutUser();
+        navigate('/')
+      }catch(error){
+        console.log(error);
+      }
+    } 
 
     useEffectOnce(()=> {
         Utils.mapSettingsDropdownItems(setSettings);
--- frontend/src/services/api/user/user.service.js
+++ frontend/src/services/api/user/user.service.js
@@ -5,6 +5,11 @@ class UserService {
         const response = await axios.get('/user/profile/users/suggestions');
         return response;
     }
+
+    async logoutUser(){
+        const response = await axios.get('/signout');
+        return response;
+    }
 }
 
 export const userService = new UserService();
