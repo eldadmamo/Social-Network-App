988c1a5c39d40d892d7dc21d0a4062df7f8db875
eldadstar <eldadfikre456@gmail.com>
2025-03-06 16:01:49 +0300

    post modal wrapper

--- frontend/src/redux-toolkit/store.js
+++ frontend/src/redux-toolkit/store.js
@@ -2,11 +2,13 @@ import {configureStore} from '@reduxjs/toolkit';
 import userReducer from '../redux-toolkit/reducers/user/user.reducer'
 import suggestionsReduce from '../redux-toolkit/reducers/suggestions/suggestions.reducer'
 import notificationReducer from '../redux-toolkit/reducers/suggestions/suggestions.reducer'
+import modalReducer from '../redux-toolkit/reducers/model/modal.reducer'
 
 export const store = configureStore({
     reducer: {
         user: userReducer,
         suggestions: suggestionsReduce,
-        notifications: notificationReducer
+        notifications: notificationReducer,
+        modal:modalReducer
     }
 })
