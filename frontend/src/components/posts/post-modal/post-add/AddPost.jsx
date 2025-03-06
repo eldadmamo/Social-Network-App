90ec91785a3de05e118e34a083e37246c4c033c6
eldadstar <eldadfikre456@gmail.com>
2025-03-06 19:32:34 +0300

    Color background

--- frontend/src/components/posts/post-modal/post-add/AddPost.jsx
+++ frontend/src/components/posts/post-modal/post-add/AddPost.jsx
@@ -4,11 +4,32 @@ import { useSelector } from 'react-redux'
 import '../post-add/AddPost.scss'
 import ModalBoxContent from '../modal-box-content/ModalBoxContent'
 import { FaTimes } from 'react-icons/fa'
+import { bgColors } from '../../../../services/utils/static.data'
+import Button from '../../../button/Button'
+import ModalBoxSelection from './../modal-box-content/modalBoxSelection'
+import { PostUtils } from '../../../../services/utils/post-utils.service'
 
 const AddPost = () => {
     const {gifModalIsOpen} = useSelector((state) => state.modal);
     const {loading} = useState(false);
     const [postImage] = useState('');
+    const [allowedNumberOfCharacters] = useState('100/100');
+    const [textAreaBackground, setTextAreaBackground] = useState('#ffffff');
+    const [postData, setPostData] = useState({
+        post:'',
+        bgColor: textAreaBackground,
+        privacy: '',
+        feelings: '',
+        gifUrl: '',
+        profilePicture: '',
+        image: ''
+    });
+    const [disable, setDisable] = useState(false);
+
+    const selectBackground = (bgColor) => {
+        PostUtils.selectBackground(bgColor, postData, setTextAreaBackground, setPostData, setDisable);
+    }
+
 
 
   return (
@@ -33,13 +54,17 @@ const AddPost = () => {
 
                 {!postImage && (
                     <> 
-                  <div className='modal-box-form' data-testid="modal-box-form">
-                    <div className='main'>
+                  <div 
+                  className='modal-box-form' 
+                  data-testid="modal-box-form" 
+                  style={{background: `${textAreaBackground}`}}>
+
+                    <div className='main' style={{margin: textAreaBackground !== '#ffffff'? '0 auto':''}}>
                         <div className='flex-row'>
                             <div 
                             data-testid="editable"
                             name="post"
-                            className='editable flex-item'
+                            className={`editable flex-item ${textAreaBackground !== '#ffffff'? 'textInputColor':''}`}
                             contentEditable={true}
                             data-placeholder="what's on your mind?..."
                             >
@@ -69,8 +94,31 @@ const AddPost = () => {
                   </div>
                 </>
                 )}
-                
+
+                <div className='modal-box-bg-colors'>
+                    <ul>
+                        {bgColors.map((color, index) => (
+                            <li
+                            data-testid="bg-colors"
+                            key={index}
+                            className={`${color === '#ffffff' ? 'whiteColorBorder': ''}`}
+                            style={{ backgroundColor: `${color}`}}
+                            onClick={()=> selectBackground(color)}
+                            ></li>
+                        ))}
+                    </ul>
+                </div>
+                <span className='char_count' data-testid="allowed-number">
+                    {allowedNumberOfCharacters}
+                </span>
+
+                <ModalBoxSelection/>
+
+                <div className='modal-box-button' data-testid="post-button">
+                    <Button label="Create Post" className="post-button"  disabled={true}/>
+                </div>
             </div>
+
         )}
         {gifModalIsOpen && (
             <div>Gif</div>
---
+++ frontend/src/services/utils/post-utils.service.jsx
@@ -0,0 +1,12 @@
+
+
+export class PostUtils {
+    static selectBackground(bgColor, postData, setTextAreaBackground, setPostData, setDisable){
+        postData.bgColor = bgColor;
+        setTextAreaBackground(bgColor);
+        setPostData(postData);
+        setDisable(false);
+    }
+}
+
+export const postUtils = new PostUtils
