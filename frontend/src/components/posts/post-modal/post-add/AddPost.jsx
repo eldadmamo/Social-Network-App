0de2ca2e7ca1245051579deef04137ac9f5fb9c1
eldadstar <eldadfikre456@gmail.com>
2025-03-06 16:49:07 +0300

    Post modal component

--- frontend/src/components/posts/post-modal/post-add/AddPost.jsx
+++ frontend/src/components/posts/post-modal/post-add/AddPost.jsx
@@ -3,10 +3,13 @@ import PostWrapper from '../../modal-wrappers/post-wrapper/PostWrapper'
 import { useSelector } from 'react-redux'
 import '../post-add/AddPost.scss'
 import ModalBoxContent from '../modal-box-content/ModalBoxContent'
+import { FaTimes } from 'react-icons/fa'
 
 const AddPost = () => {
     const {gifModalIsOpen} = useSelector((state) => state.modal);
     const {loading} = useState(false);
+    const [postImage] = useState('');
+
 
   return (
     <>
@@ -27,6 +30,46 @@ const AddPost = () => {
                 </div>
                 <hr/>
                 <ModalBoxContent/>
+
+                {!postImage && (
+                    <> 
+                  <div className='modal-box-form' data-testid="modal-box-form">
+                    <div className='main'>
+                        <div className='flex-row'>
+                            <div 
+                            data-testid="editable"
+                            name="post"
+                            className='editable flex-item'
+                            contentEditable={true}
+                            data-placeholder="what's on your mind?..."
+                            >
+
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </>
+                )}
+                {postImage && (
+                    <> 
+                  <div className='modal-box-image-form'>
+                      <div 
+                        data-testid="post-editable"
+                        name="post"
+                        className='post-input flex-item'
+                        contentEditable={true}
+                        data-placeholder="what's on your mind?..."
+                        ></div>
+                        <div className='image-display'>
+                            <div className='image-delete-btn' data-testid="image-delete-btn">
+                                <FaTimes/>
+                            </div>
+                            <img data-testid="post-image" className='post-image' src='' alt=''/>
+                        </div>
+                  </div>
+                </>
+                )}
+                
             </div>
         )}
         {gifModalIsOpen && (
