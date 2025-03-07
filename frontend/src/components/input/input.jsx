a2d14f02fe19fe30cc0cf7b09d20425675997cc8
eldadstar <eldadfikre456@gmail.com>
2025-03-07 17:35:42 +0300

    File Selection

--- frontend/src/components/input/input.jsx
+++ frontend/src/components/input/input.jsx
@@ -1,30 +1,54 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 import PropTypes from 'prop-types'
 import '../input/input.scss'
 
-const Input = ({id, name, type, value, className, labelText, placeholder, handleChange, style}) => {
-  return (
-    <>
+// const Input = ({id, name, type, value, className, labelText, placeholder, handleChange, style}) => {
+  // return (
+  //   <>
+  //     <div className='form-row'>
+  //       {labelText && (
+  //           <label htmlFor={name} className='form-label'>{labelText}</label>
+  //       )}
+
+  //       <input 
+  //         id={id}
+  //         name={name}
+  //         type={type}
+  //         value={value}
+  //         onChange={handleChange}
+  //         placeholder={placeholder}
+  //         className={`form-input ${className}`}
+  //         style={style}
+  //         autoComplete='false'
+  //       /> 
+  //     </div>
+  //   </>
+  // )
+// }
+
+const Input = forwardRef((props, ref) => (
       <div className='form-row'>
-        {labelText && (
-            <label htmlFor={name} className='form-label'>{labelText}</label>
+        {props.labelText && (
+            <label htmlFor={name} className='form-label'>{props.labelText}</label>
         )}
 
         <input 
-          id={id}
-          name={name}
-          type={type}
-          value={value}
-          onChange={handleChange}
-          placeholder={placeholder}
-          className={`form-input ${className}`}
-          style={style}
+          ref={ref}
+          id={props.id}
+          name={props.name}
+          type={props.type}
+          value={props.value}
+          onChange={props.handleChange}
+          placeholder={props.placeholder}
+          onClick={props.onClick}
+          onFocus={props.onFocus}
+          onBlur={props.onBlur}
+          className={`form-input ${props.className}`}
+          style={props.style}
           autoComplete='false'
         /> 
       </div>
-    </>
-  )
-}
+));
 
 Input.propTypes = {
     name: PropTypes.string.isRequired,
@@ -35,6 +59,9 @@ Input.propTypes = {
     className: PropTypes.string,
     placeholder: PropTypes.string,
     handleChange: PropTypes.func,
+    onClick: PropTypes.func,
+    onFocus: PropTypes.func,
+    onBlur: PropTypes.func,
     style: PropTypes.object 
 }
 
--- frontend/src/components/posts/post-modal/modal-box-content/modalBoxSelection.jsx
+++ frontend/src/components/posts/post-modal/modal-box-content/modalBoxSelection.jsx
@@ -4,14 +4,24 @@ import gif from '../../../../assets/images/gif.png'
 import feeling from '../../../../assets/images/feeling.png'
 import Input from '../../../input/input'
 import useDetectOutsideClick from '../../../../hooks/useDetectOutsideClick'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Feelings from '../../../feelings/Feelings'
 
 const ModalBoxSelection = () => {
 	const {feelingIsOpen} = useSelector((state) => state.modal);
+	const {post} = useSelector((state) => state.post);
 	const feelingsRef = useRef(null);
+	const fileInputRef = useRef();
 	const [toggleFeelings,setToggleFeelings] = useDetectOutsideClick(feelingsRef, feelingIsOpen);
+	const dispatch = useDispatch();
 
+	const fileInputClicked = () => {
+		fileInputRef.current.click();
+	}
+
+	const handleFileChange = (event) => {
+		console.log(event.taget.files[0])
+	}
 
   return (
     <>
@@ -22,8 +32,19 @@ const ModalBoxSelection = () => {
 	)}
     <div className="modal-box-selection" data-testid="modal-box-selection">
 	  <ul className="post-form-list" data-testid="list-item">
-		<li className="post-form-list-item image-select">
-			<Input name="image"  type="file" className="file-input" />
+		<li className="post-form-list-item image-select" onClick={fileInputClicked}>
+			<Input 
+			name="image" 
+			ref={fileInputRef} 
+			type="file" 
+			className="file-input" 
+			onClick={()=> {
+				if (fileInputRef.current){
+					fileInputRef.current.value = null;
+				}
+			}}
+			handleChange={handleFileChange}
+			/>
 			<img src={photo} alt="" /> Photo
 		</li>
 		<li className="post-form-list-item">
