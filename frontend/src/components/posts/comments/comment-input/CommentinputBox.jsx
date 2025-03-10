8ad31a202fd80c71e183c120f904761723e5bfce
eldadstar <eldadfikre456@gmail.com>
2025-03-10 18:19:21 +0300

    input box components

---
+++ frontend/src/components/posts/comments/comment-input/CommentinputBox.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useRef, useState } from 'react'
+import PropTypes from 'prop-types'
+import Input from '../../../../components/input/input'
+import '../../../../components/posts/comments/comment-input/CommentInputBox.scss'
+import { useDispatch, useSelector } from 'react-redux'
+import { Utils } from '../../../../services/utils/utils.service'
+import { cloneDeep } from 'lodash'
+import { socketService } from '../../../../services/socket/socket.service'
+import { postService } from '../../../../services/api/post/post.service'
+
+const CommentinputBox = ({post}) => {
+    const {profile} = useSelector((state) => state.profile)
+    const [comment, setComment] = useState('')
+    const commentInputRef = useRef(null);
+    const dispatch = useDispatch();
+
+    const submitComment = async (event) => {
+        event.preventDefault();
+        try{
+            post = cloneDeep(post);
+            post.commentsCount +=1;
+            const commentBody = {
+                userTo: post?.userId,
+                postId: post?._id,
+                comment: comment.trim(),
+                commentsCount: post.commentsCount,
+                profilePicture: profile?.profilePicture
+            };
+            socketService?.socket?.emit('comment', commentBody);
+            await postService.addComment(commentBody);
+            setComment('');
+        }catch(error){
+            Utils.dispatchNotification(error.response.data.message, 'error', dispatch);
+        }
+    }
+
+    useEffect(()=> {
+        if (commentInputRef?.current){
+            commentInputRef.current.focus()
+        }
+    },[])
+
+  return (
+    <div className='comment-container' data-testid="comment-input">
+        <form className='comment-form' onSubmit={submitComment}>
+            <Input 
+            ref={commentInputRef}
+            name="comment"
+            type="text"
+            value={comment}
+            labelText=""
+            className="comment-input"
+            placeholder="Write a comment..."
+            handleChange={(event)=> setComment(event.target.value)}
+            />
+        </form>
+    </div>
+  )
+}
+
+CommentinputBox.propTypes = {
+    post: PropTypes.object
+}
+
+export default CommentinputBox
