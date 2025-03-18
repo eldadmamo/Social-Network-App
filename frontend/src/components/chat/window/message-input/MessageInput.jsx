3c7b9cf925cedde9f0b8a05bbf8d6877a113e55f
eldadstar <eldadfikre456@gmail.com>
2025-03-18 17:00:51 +0300

    image preview

--- frontend/src/components/chat/window/message-input/MessageInput.jsx
+++ frontend/src/components/chat/window/message-input/MessageInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import Input from '../../../input/input'
 import Button from '../../../button/Button'
@@ -9,17 +9,39 @@ import feelings from '../../../../assets/images/feeling.png'
 import './MessageInput.scss'
 import loadable from '@loadable/component'
 import GiphyContainer from '../../giphy-container/GiphyContainer'
+import ImagePreview from '../../image-preview/ImagePreview'
+import { ImageUtils } from './../../../../services/utils/image-utils.service';
 
 const EmojiPickerComponent = loadable(()=> import('./EmojiPicker'), {
   fallback: <p id='loading'>Loading...</p>
+
 })
 
 const MessageInput = ({ setChatMessage }) => {
   const [showEmojiContainer, setShowEmojiContainer] = useState(false)
   const [showGifContainer, setShowGifContainer] = useState(false);
+  const [showImagePreview, setShowImagePreview] = useState(false)
+  const [file, setFile] = useState();
+
+
+  const fileInputRef = useRef();
 
   const handleGiphyClick =() => {
 
+  }
+
+  const addToPreview = async (file) => {
+    ImageUtils.checkFile(file, 'image')
+    setFile(URL.createObjectURL(file));
+    const result = await ImageUtils.readAsBase64(file);
+    
+    setShowImagePreview(!showImagePreview)
+    setShowEmojiContainer(false);
+    setShowGifContainer(false);
+  }
+
+  const fileInputClicked = () => {
+    fileInputRef.current.click();
   }
 
     return (
@@ -32,7 +54,7 @@ const MessageInput = ({ setChatMessage }) => {
         pickerStyle={{width: '352px', height: '447px'}}
         />
       )}
-      {!showGifContainer && 
+      {showGifContainer && 
       <GiphyContainer
       handleGiphyClick={handleGiphyClick}
     
@@ -40,28 +62,49 @@ const MessageInput = ({ setChatMessage }) => {
       
       }
       <div className="chat-inputarea" data-testid="chat-inputarea">
+        {showImagePreview && (
+          <ImagePreview 
+          image={file}
+          onRemoveImage={()=> {
+            setFile('')
+            setShowImagePreview(!showImagePreview)
+          }}
+          />
+        )}
         <form className="chat-form">
           <ul className="chat-list" style={{borderColor: '#50b5ff'}}>
-            <li className="chat-list-item"  onClick={()=> {
+            <li className="chat-list-item"  
+            onClick={()=> {
+              fileInputClicked();
               setShowEmojiContainer(false)
               setShowGifContainer(false)
             }}>
-              <Input id="image" name="image" type="file" className="file-input" placeholder="Select file" />
-              <img src={photo} alt="Photo" />
+              <Input 
+              ref={fileInputRef} id="image" name="image" type="file" className="file-input" placeholder="Select file" 
+              onClick={()=> {
+                if(fileInputRef.current){
+                  fileInputRef.current.value = null;
+                }
+              }}
+              handleChange={(event) => addToPreview(event.target.files[0])}
+              />
+              <img src={photo} alt="" />
             </li>
             <li className="chat-list-item" 
             onClick={()=> {
+              setShowGifContainer(!showGifContainer)
               setShowEmojiContainer(false)
-              setShowGifContainer(!showEmojiContainer)
+              setShowImagePreview(false)
             }}>
-              <img src={gif} alt="GIF" />
+              <img src={gif} alt="" />
             </li>
             <li className="chat-list-item" 
             onClick={()=> {
-              setShowGifContainer(false)
               setShowEmojiContainer(!showEmojiContainer)
+              setShowGifContainer(false)
+              setShowImagePreview(false)
             }}>
-              <img src={feelings} alt="Feelings" />
+              <img src={feelings} alt="" />
             </li>
           </ul>
           <Input id="message" name="message" type="text" className="chat-input" placeholder="Enter your message..." />
